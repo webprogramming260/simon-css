26642503152aa15ab77f4f3ad155465fdae95bef
Lee <lee.jensen@agilix.com>
2022-11-09 15:53:07 -0700

    fix() deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+mkdir -p ../webprogramming260.github.io/simon-css
 cp *.{html,css} ../webprogramming260.github.io/simon-css
 cd ../webprogramming260.github.io
 git commit -am "updating simon-css project"
