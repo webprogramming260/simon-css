b50297e70e9c8557be1b86d6d2759b5de4173d64
Lee <lee.jensen@agilix.com>
2022-11-10 09:32:05 -0700

    enhance(deploy) to add new files and delete removed ones

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
+rm -rf ../webprogramming260.github.io/simon-css
 mkdir -p ../webprogramming260.github.io/simon-css
 cp *.{html,css} ../webprogramming260.github.io/simon-css
 cd ../webprogramming260.github.io
-git commit -am "updating simon-css project"
+git add .
+git commit -am "deploying simon-css project"
 git push
