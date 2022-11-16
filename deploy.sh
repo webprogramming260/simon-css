736ea3989e43beb97d6c66b33e9f14951c3266aa
Lee <lee.jensen@agilix.com>
2022-11-16 15:55:30 -0700

    add(favicon)

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 rm -rf ../webprogramming260.github.io/simon-css
 mkdir -p ../webprogramming260.github.io/simon-css
-cp *.{html,css} ../webprogramming260.github.io/simon-css
+cp *.{html,css,ico} ../webprogramming260.github.io/simon-css
 cd ../webprogramming260.github.io
 git add .
 git commit -am "deploying simon-css project"
